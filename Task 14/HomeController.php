88e5dc114a3b85048416ffde442b108ff838135b
Petani Kode <contoh@petanikode.com>
2025-06-03 21:02:05 +0700

    Task 14 Laravel membuat home pagination

---
+++ Task 14/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $products = Product::paginate(3);
+        return view('home', compact('products'));
+    }
+
+    public function cart()
+    {
+        return view('cart');
+    }
+}
---
+++ Task 14/components/footer.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <footer class="mt-5">
+        <div class="container text-center">
+            <p>&copy; 2025 Toko Online Saya. Hak Cipta Dilindungi.</p>
+            <p>
+                <a href="#" class="text-decoration-none text-dark me-2">Kebijakan Privasi</a> |
+                <a href="#" class="text-decoration-none text-dark ms-2">Syarat & Ketentuan</a>
+            </p>
+        </div>
+    </footer>
+</div>
